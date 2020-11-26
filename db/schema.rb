c08a860d6e551a519376eefaaf25c2dd61a6a659
arczee2020 <arczee2020@gmail.com>
2020-11-27 00:02:12 +0500

    Add the default value of the user

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 2020_11_23_181836) do
     t.string "name"
     t.string "email"
     t.string "password"
-    t.string "role"
+    t.string "role", default: "normal_user"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
