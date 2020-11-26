81622408aaa892fa9419a173a001bdbb326862b8
arczee2020 <arczee2020@gmail.com>
2020-11-26 21:22:08 +0500

    Add routes of sign up or login and add container fluid class in body

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "ecommerces#index"
-  get '/signup', to: 'auths#signup', as: 'signup'
+  get '/signup', to: 'auths#sign_up', as: 'signup'
   get '/login', to: 'auths#login', as: 'login'
   get '/forget_password', to: 'auths#forget_password', as: 'forget_password'
 
