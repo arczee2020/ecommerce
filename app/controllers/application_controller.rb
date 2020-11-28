96f06a8f04f658c83a3a4df1cff50890035cc7b9
arczee2020 <arczee2020@gmail.com>
2020-11-28 13:04:02 +0500

    Refactor code of the auth controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :initialize_session
 
   def initialize_session
-    session[:current_user] ||= []
+    session[:current_user] ||= {}
 
     if session[:current_user].count == 0
       file = Dir.glob("#{Rails.root}/public/product/data.csv")
